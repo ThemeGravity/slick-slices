678a0e76e68446ca6221cb884132ad32c1a6992a
ThemeGravity <themegravity@gmail.com>
2020-11-22 14:43:27 +0100

    Init repo

---
+++ gatsby/src/components/ToppingsFilter.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { graphql, useStaticQuery, Link } from 'gatsby';
+import styled from 'styled-components';
+
+const ToppingsStyles = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  margin-bottom: 4rem;
+  a {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    grid-gap: 0 1rem;
+    align-items: center;
+    padding: 5px;
+    background: var(--grey);
+    border-radius: 2px;
+    .count {
+      background: white;
+      padding: 2px 5px;
+    }
+    &[aria-current='page'] {
+      background: var(--yellow);
+    }
+  }
+`;
+
+const countPizzasInToppings = (pizzas) => {
+  const counts = pizzas
+    .map((pizza) => pizza.toppings)
+    .flat()
+    .reduce((acc, topping) => {
+      const existingToppings = acc[topping.id];
+      if (existingToppings) {
+        existingToppings.count += 1;
+      } else {
+        acc[topping.id] = {
+          id: topping.id,
+          name: topping.name,
+          count: 1,
+        };
+      }
+      return acc;
+    }, {});
+  const sortedToppings = Object.values(counts).sort((a, b) => b.count - a.count);
+  return sortedToppings;
+};
+
+const ToppingsFilter = ({ activeTopping }) => {
+  const { toppings, pizzas } = useStaticQuery(graphql`
+    query {
+      toppings: allSanityTopping {
+        nodes {
+          id
+          name
+          vegetarian
+        }
+      }
+      pizzas: allSanityPizza {
+        nodes {
+          toppings {
+            id
+            name
+          }
+        }
+      }
+    }
+  `);
+  const toppingsWithCounts = countPizzasInToppings(pizzas.nodes);
+  return (
+    <ToppingsStyles>
+      <Link to="/pizzas">
+        <span className="name">All</span>
+        <span className="count">{pizzas.nodes.length}</span>
+      </Link>
+      {toppingsWithCounts.map((topping) => (
+        <Link to={`/topping/${topping.name}`} key={topping.id}>
+          <span className="name">{topping.name}</span>
+          <span className="count">{topping.count}</span>
+        </Link>
+      ))}
+    </ToppingsStyles>
+  );
+};
+
+export default ToppingsFilter;
